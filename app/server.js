a42b061ec50ef4334af39c61de77de03cf5e6e50
Amol Jagtap <amoljagtap17@gmail.com>
2019-05-19 20:51:34 +0530

    REDUX-END-TO-END: Isomorphic rendering

--- app/server.js
+++ app/server.js
@@ -1,12 +1,13 @@
 import express from 'express'
-import template from './template'
+import render from './serverRender'
 
 const app = express()
 
 app.use('/assets', express.static('assets'))
 
-const renderClient = (req, res) => res.send(template)
+// const renderClient = (req, res) => res.send(template)
+// app.use(renderClient)
 
-app.use(renderClient)
+app.use(render)
 
 app.listen(9999)
--- app/template.js
+++ app/template.js
@@ -1,4 +1,4 @@
-export default `
+export default html => `
   <!DOCTYPE html>
   <html lang="en">
     <head>
@@ -13,7 +13,7 @@ export default `
       <link rel="stylesheet" href="/assets/app.css" />
     </head>
     <body>
-      <div id="app"></div>
+      <div id="app">${html}</div>
     </body>
     <script src="/assets/bundle.js"></script>
   </html>
